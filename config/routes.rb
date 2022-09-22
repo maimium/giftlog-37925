d6abb6629f5185640e198f422e2e4b2b2e6c4dfd
maimium <mamopoyoo@yahoo.co.jp>
2022-09-22 22:56:14 +0900

    ユーザー管理機能
    deviceを用いてユーザー登録ができるようにしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
-  root "recommend_gifts#index"
+  devise_for :users
+
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
+
+  resources :received_gifts
+  resources :gave_gifts
+  resources :recommend_gifts
  
 end
